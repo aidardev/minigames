906966a50a15279807a50333657dec40f02361c5
aidardev <aidarwebdev@gmail.com>
2026-09-15 20:13:20 +0500

    feat: configure ESLint with TypeScript and Unicorn

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import unicorn from "eslint-plugin-unicorn";
+import tseslint from "typescript-eslint";
+
+export default defineConfig({
+    ignores: ["dist/**", "node_modules/**", "coverage/**"],
+
+    files: ["**/*.ts"],
+
+    extends: [js.configs.recommended, tseslint.configs.recommended, unicorn.configs.recommended],
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+    },
+
+    linterOptions: {
+        noInlineConfig: true,
+        reportUnusedDisableDirectives: "error",
+    },
+
+    rules: {
+        "@typescript-eslint/no-explicit-any": "error",
+        "no-console": "error",
+
+        "unicorn/prevent-abbreviations": "off",
+    },
+});
