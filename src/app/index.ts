3b8af6c5730c40b403c777fe560d53a0d33589b4
aidardev <aidarwebdev@gmail.com>
2026-09-16 23:02:34 +0500

    feat: render header in application shell

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,4 @@
+import { Header } from '@/components/header/header';
 import { HomePage } from '@/pages/home/home';
 import { LibraryPage } from '@/pages/library/library';
 import { Router } from './router';
@@ -6,9 +7,16 @@ export function startApp(): void {
     const appRoot = document.createElement('div');
     appRoot.className = 'app';
 
+    const header = new Header();
+
+    const pageOutlet = document.createElement('div');
+    pageOutlet.className = 'app__outlet';
+
+    appRoot.append(header.element, pageOutlet);
+
     document.body.prepend(appRoot);
 
-    new Router(appRoot, {
+    new Router(pageOutlet, {
         '/': HomePage,
         '/library': LibraryPage,
     }).start();
