385c45990f1b8b8be03cd2704416377b530359e8
aidardev <aidarwebdev@gmail.com>
2026-09-16 23:01:57 +0500

    feat: add header semantic markup

---
+++ src/components/header/header.ts
@@ -0,0 +1,45 @@
+import closeIcon from '@/assets/icons/close.svg?raw';
+import burgerIcon from '@/assets/icons/menu.svg?raw';
+import logo from '@/assets/images/logo.svg';
+import { BaseComponent } from '../base-component';
+
+export class Header extends BaseComponent {
+    public constructor() {
+        super('header', 'header');
+
+        this.element.innerHTML = /* HTML */ `
+            <div class="header__inner container">
+                <a href="#" class="header__logo logo logo--dark">
+                    <img src="${logo}" alt="Logo" class="logo__img" width="32" height="32">
+                    <span class="logo__text">MiniGames</span>
+                </a>
+                <div class="header__menu">
+                    <div class="header__mobile-topbar">
+                        <a href="#" class="header__logo-mobile logo logo--white">
+                            <img src="${logo}" alt="Logo" class="logo__img" width="32" height="32">
+                            <span class="logo__text">MiniGames</span>
+                        </a>
+                        <button class="header__close" type="button">${closeIcon}</button>
+                    </div>
+                    <nav class="header__navbar navbar">
+                        <ul class="navbar__list list-unstyled">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Library</a></li>
+                            <li><a href="#">Tournaments</a></li>
+                            <li><a href="#">Community</a></li>
+                        </ul>
+                    </nav>
+                    <div class="header__btns">
+                        <button class="header__btn btn btn--outline-primary" type="button">
+                            Log In
+                        </button>
+                        <button class="header__btn btn btn--primary" type="button">Sign Up</button>
+                    </div>
+                </div>
+                <button class="header__hamburger-btn btn btn--icon" type="button">
+                    ${burgerIcon}
+                </button>
+            </div>
+        `;
+    }
+}
