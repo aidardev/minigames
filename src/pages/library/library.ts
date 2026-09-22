68c5273535ce17b7c0a263abb6aed122b57c5226
aidardev <aidarwebdev@gmail.com>
2026-09-22 21:35:45 +0500

    feat: mount PageTitle to library page

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,10 +1,17 @@
 import { BaseComponent } from '@/components/base-component';
+import { PageTitle } from '@/components/page-title/page-title';
 import { CatalogSection } from './blocks/catalog/catalog';
 
 export class LibraryPage extends BaseComponent {
     public constructor() {
         super('main', 'library-page');
 
-        this.element.append(new CatalogSection().element);
+        this.element.append(
+            new PageTitle({
+                title: 'Game Library',
+                subtitle: 'Browse our collection of casual mini-games',
+            }).element,
+            new CatalogSection().element,
+        );
     }
 }
